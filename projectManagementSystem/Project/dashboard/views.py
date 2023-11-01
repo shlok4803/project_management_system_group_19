3c006c8e947d542808176298d7344fb57e8e743d
shlok4803 <shlokmansata48@gmail.com>
2023-11-02 04:08:52 +0530

    Finished dashboard view

--- projectManagementSystem/Project/dashboard/views.py
+++ projectManagementSystem/Project/dashboard/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from accounts.models import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from accounts.views import *
 
-# Create your views here.
+# Owner, PM and Employee
+@login_required
 def dashboard(request):
+    
+    if request.user.is_staff:
+        messages.error(request, "Staff accounts cannot be used.")
+        return redirect('Logout')
+    
+    if not request.session.get('employee'):
+        messages.error(request, "Session logged out due to inactivity.")
+        return redirect('Logout')
+    
     user = request.user 
     if isinstance(user,employee):
         return render(request,'dashboard_employee.html')
@@ -12,6 +26,9 @@ def dashboard(request):
     
     elif isinstance(user,owner):
         return render(request,'dashboard_owner.html')
+    
+    elif  user != 'employee' and user !='manager' and user != 'owner':
+        raise PermissionDenied
     
     
     
