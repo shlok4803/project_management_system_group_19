b1b649d1b16446778f35bf14fc8f61305da5f360
shlok4803 <shlokmansata48@gmail.com>
2023-11-02 03:15:02 +0530

    Adding dashboard view

--- projectManagementSystem/Project/dashboard/views.py
+++ projectManagementSystem/Project/dashboard/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from accounts.models import *
 
 # Create your views here.
+def dashboard(request):
+    user = request.user 
+    if isinstance(user,employee):
+        return render(request,'dashboard_employee.html')
+    
+    elif isinstance(user,manager):
+        return render(request,'dashboard_manager.html')
+    
+    elif isinstance(user,owner):
+        return render(request,'dashboard_owner.html')
+    
+    
+    
+    
+        
+        
+        
