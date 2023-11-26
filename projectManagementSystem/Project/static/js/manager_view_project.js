e089ffb342ff27ce72c782413f06e03ddae3f100
Dev Hingu <devhingu8103@gmail.com>
2023-11-26 15:53:48 +0530

    manager navbar and header set

--- projectManagementSystem/Project/static/js/manager_view_project.js
+++ projectManagementSystem/Project/static/js/manager_view_project.js
@@ -39,85 +39,6 @@ function getCSRFToken() {
   }
   return null;
 }
-// const descbox2=document.querySelector(".description-box");
-
-// function showtask2(row){
-//   if(descbox2.style.display=="none"){
-//       descbox2.style.display="flex";
-//       document.querySelector(".tasktitle1").textContent=`Task ${row}`;      
-//       document.querySelector("#taskdetails").textContent="Hide Details";
-//       document.querySelector(".desc-box").style.justifyContent="space-between";
-//   }  
-//   else{
-//       descbox2.style.display="none";    
-//       document.querySelector(".desc-box").style.justifyContent="center";
-//       document.querySelector("#taskdetails").textContent="Task Details";
-//   }
-// }
-
-// const descbox=document.querySelector(".details-container");
-
-// var totalRowCount = 0;
-// var rowCount = 0;
-// var table = document.getElementById("maintable");
-// var table = document.getElementById("maintable");
-// var rows = table.getElementsByTagName("tr");
-
-// for (var i = 0; i < rows.length; i++) {
-//     totalRowCount++;
-//     if (rows[i].getElementsByTagName("td").length > 0) {
-//         rowCount++;
-//     }
-// }
-// function showtask(row,btn){
-//     // console.log(row);
-//     descbox2.style.display="none"; 
-//      if(btn.textContent=="View"){
-//         document.querySelector(".desc-box").style.justifyContent="center";
-//         document.querySelector("#taskdetails").textContent="Task Details";
-//          descbox.style.display="flex";
-//     for(let j=1;j<=rowCount;j++){
-//             if(j==row)
-//             continue;
-//     const tablerow=document.getElementById(`tablerow${j}`);
-//     const btn2=tablerow.querySelector(".button-tag");
-//     btn2.textContent="View";
-//     }
-//     btn.textContent="Hide";
-//     // console.log(btn);
-// }
-
-// else{
-//     descbox.style.display="none";
-//     btn.textContent="View";
-// }
-// }
-// document.querySelector("#taskdetails").addEventListener('click',()=>{
-//     for(let j=1;j<=rowCount;j++){
-//         const tablerow=document.getElementById(`tablerow${j}`);
-//         const btn2=tablerow.querySelector(".button-tag");
-//         if(btn2.textContent=="Hide"){
-//             showtask2(j);
-//         }
-//     }
-// });
-// document.querySelector("#deletebtn").addEventListener('click',()=>{
-//     for(let j=1;j<=rowCount;j++){
-//         const tablerow=document.getElementById(`tablerow${j}`);
-//         const btn2=tablerow.querySelector(".button-tag");
-//         if(btn2.textContent=="Hide"){
-//             deleteRow(btn2);
-//             descbox.style.display="none";
-//             descbox2.style.display="none";
-
-//         }
-//     }
-// });
-//  for(let j=1;j<=rowCount;j++){
-//     const tablerow=document.getElementById(`tablerow${j}`);
-//     const btn2=tablerow.querySelector(".button-tag");
-//     btn2.onclick=function() { console.log(tablerow); showtask(j,btn2);};
-// }
 
 document.querySelector(".statuscheck").addEventListener('click',()=>{
   if(document.querySelector(".statuscheck").textContent=="Submitted for review"){
@@ -150,15 +71,17 @@ function myFunction(filter) {
     filter="Submitted for review";
   }
   filter=filter.toUpperCase();
-  if(filter==="SHOW ALL" || filter==='#'){
-    let filter1="PENDING";
+  console.log(filter);
+  if(filter==="SHOW ALL"){
+    let filter1="IN PROGRESS";
     let filter2="COMPLETED";
+    // console.log(filter1);
     let filter3="SUBMITTED FOR REVIEW";
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[1];
       if (td) {
         txtValue = td.textContent || td.innerText;
-        if ((txtValue.toUpperCase().indexOf(filter1) > -1) || (txtValue.toUpperCase().indexOf(filter2)>-1) || (txtValue.toUpperCase().indexOf(filter3) > -1))  {
+        if ((txtValue.toUpperCase().indexOf(filter1) > -1) || (txtValue.toUpperCase().indexOf(filter2)>-1))  {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
@@ -183,7 +106,7 @@ function myFunction(filter) {
 function getOption() {
   selectElement = document.querySelector('#select1');
   output = selectElement.value;
-  console.log(output);
+  // console.log(output);
   myFunction(output);
 }
 
